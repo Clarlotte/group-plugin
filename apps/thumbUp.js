6eb654f41af87085c07e9371e7ca9738a75627e4
Clarlotte <lzy_zm@outlook.com>
2024-04-30 14:12:00 +0000

    优化细节
    Signed-off-by: Clarlotte <lzy_zm@outlook.com>

--- apps/thumbUp.js
+++ apps/thumbUp.js
@@ -24,10 +24,7 @@ export class thumbUp extends plugin {
             e.reply('该群群管功能未开启，请发送开启群管启用该群的群管功能')
             return false
         }
-        let isSuccess = await e.bot.sendApi("send_like", {
-            user_id: e.user_id,
-            times: 20,
-        })
+        let isSuccess = await e.bot.pickFriend(e.user_id).thumbUp(10)
         if (isSuccess.status == 'ok') {
             e.reply(`我已成功赞你10次，记得回赞哟`)
         } else {
