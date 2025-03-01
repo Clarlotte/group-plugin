f581d75c42eeb915f6e097e41709137e2ef80dac
Clarlotte <lzy_zm@outlook.com>
2025-03-01 11:20:16 +0000

    更换天气api，优化细节
    Signed-off-by: Clarlotte <lzy_zm@outlook.com>

--- apps/weatherInquiry.js
+++ apps/weatherInquiry.js
@@ -5,6 +5,7 @@ import common from '../common/common.js'
 import axios from 'axios'
 
 export class weatherInquiry extends plugin {
+    dirPath = path.resolve("./plugins/group-plugin/data")
     constructor() {
         super({
             name: '天气查询',
@@ -50,49 +51,51 @@ export class weatherInquiry extends plugin {
         if (province.includes(cityName) || city.includes(cityName) || district.includes(cityName)) {//存在 cityName
             //获取城市 cityName 格式化地址
             let formatted_address = gaode_response.data.geocodes[0].formatted_address
-            //获取城市 cityName 经纬度坐标Z
+            //获取城市 cityName 经纬度坐标
             let location = gaode_response.data.geocodes[0].location
-            const weather_url = `https://api.caiyunapp.com/v2.6/${weather_key}/${location}/weather?alert=true`
+            const weather_url = `https://devapi.qweather.com/v7/weather/now?location=${location}&key=${weather_key}`
             const weather_response = await axios.get(weather_url)
             //获取城市 cityName 当前实况天气
-            const weather_data = weather_response.data.result.realtime
+            console.log(weather_response.data)
+            const weather_data = weather_response.data.now
+            console.log(weather_data)
             //获取城市 cityName 预警信息
-            const weather_alert = weather_response.data.result.alert
+            // const weather_alert = weather_response.data.result.alert
             //获取城市 cityName 风速
-            const wind_speed = weather_data.wind.speed
+            const wind_speed = weather_data.windSpeed
             //获取城市 cityName 风向角度
-            const wind_direction = weather_data.wind.direction
+            const wind_direction = weather_data.wind360
             //将获取到的风速风向角度转化为风力等级
             const wind_level = weather.get_wind_level(wind_speed, wind_direction)
             //获取城市 cityName 天气情况
-            const skycon_data = common.readJsonFile('./plugins/group-plugin/config/weather_skycon.json')
-            const weather_condition = skycon_data[weather_data.skycon]
-            if (weather_alert == null) {
-                let msg = [
-                    `${formatted_address}当前天气如下：\n`,
-                    `当前天气：${weather_condition}\n`,
-                    `温度：${weather_data.temperature}℃\n`,
-                    `体感温度：${weather_data.apparent_temperature}℃\n`,
-                    `相对湿度：${Math.round(weather_data.humidity * 100)}%\n`,
-                    `风力：${wind_level}\n`,
-                    `空气质量：${weather_data.air_quality.description.chn}  ${weather_data.air_quality.aqi.chn}\n`,
-                    `本地降水强度：${weather_data.precipitation.local.intensity}mm/hr`,
-                ]
-                e.reply(msg)
-                e.reply(`预警信息：${weather_alert.content[0].description}`)
-            } else {
-                let msg = [
-                    `${formatted_address}当前天气如下：\n`,
-                    `当前天气：${weather_condition}\n`,
-                    `温度：${weather_data.temperature}℃\n`,
-                    `体感温度：${weather_data.apparent_temperature}℃\n`,
-                    `相对湿度：${Math.round(weather_data.humidity * 100)}%\n`,
-                    `风力：${wind_level}\n`,
-                    `空气质量：${weather_data.air_quality.description.chn}  ${weather_data.air_quality.aqi.chn}\n`,
-                    `本地降水强度：${weather_data.precipitation.local.intensity}mm/hr`,
-                ]
-                e.reply(msg, true)
-            }
+            // const skycon_data = common.readJsonFile('./plugins/group-plugin/config/weather_skycon.json')
+            const weather_condition = weather_data.text
+            // if (weather_alert == null) {
+            let msg = [
+                `${formatted_address}当前天气如下：\n`,
+                `当前天气：${weather_condition}\n`,
+                `温度：${weather_data.temp}℃\n`,
+                `体感温度：${weather_data.feelsLike}℃\n`,
+                `相对湿度：${weather_data.humidity}%\n`,
+                `风力：${wind_level}\n`,
+                // `空气质量：${weather_data.air_quality.description.chn}  ${weather_data.air_quality.aqi.chn}\n`,
+                `本地降水强度：${weather_data.precip}mm/hr`,
+            ]
+            e.reply(msg)
+            //e.reply(`预警信息：${weather_alert.content[0].description}`)
+            // } else {
+            //     let msg = [
+            //         `${formatted_address}当前天气如下：\n`,
+            //         `当前天气：${weather_condition}\n`,
+            //         `温度：${weather_data.temperature}℃\n`,
+            //         `体感温度：${weather_data.apparent_temperature}℃\n`,
+            //         `相对湿度：${Math.round(weather_data.humidity * 100)}%\n`,
+            //         `风力：${wind_level}\n`,
+            //         `空气质量：${weather_data.air_quality.description.chn}  ${weather_data.air_quality.aqi.chn}\n`,
+            //         `本地降水强度：${weather_data.precipitation.local.intensity}mm/hr`,
+            //     ]
+            //     e.reply(msg, true)
+            // }
         } else {
             e.reply(`无法查询到该城市的实况天气，请检查是否拼写正确`, true)
         }
