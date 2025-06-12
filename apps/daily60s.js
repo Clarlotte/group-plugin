a25e2503ffa1dc4440a01ed15227954979a40f43
Clarlotte <lzy_zm@outlook.com>
2025-06-12 07:43:17 +0000

    update apps/daily60s.js.
    Signed-off-by: Clarlotte <lzy_zm@outlook.com>

--- apps/daily60s.js
+++ apps/daily60s.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Clarlotte
+ * @Date: 2025-06-10 19:49:08
+ * @LastEditors: Clarlotte
+ * @LastEditTime: 2025-06-12 13:19:26
+ * @FilePath: /root/Yunzai/plugins/group-plugin/apps/daily60s.js
+ * @Descripttion: 
+ */
 import plugin from '../../../lib/plugins/plugin.js'
 import common from '../common/common.js'
 import axios from 'axios'
