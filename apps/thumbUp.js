6cf059ad3575702268a2fbe8882cc5894c5fec87
Clarlotte <lzy_zm@outlook.com>
2025-06-12 07:45:04 +0000

    update apps/thumbUp.js.
    Signed-off-by: Clarlotte <lzy_zm@outlook.com>

--- apps/thumbUp.js
+++ apps/thumbUp.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Clarlotte
+ * @Date: 2025-06-10 19:49:08
+ * @LastEditors: Clarlotte
+ * @LastEditTime: 2025-06-12 13:27:04
+ * @FilePath: /root/Yunzai/plugins/group-plugin/apps/thumbUp.js
+ * @Descripttion: 
+ */
 import common from "../common/common.js";
 import path from "path";
 
@@ -27,33 +35,34 @@ export class thumbUp extends plugin {
     }
     let userId = e.user_id;
     // 判断用户是否是好友
-    if (this.checkFriend) {
-      let isFriend = await (e.bot ?? Bot).fl.get(userId);
-      if (!isFriend) {
-        let msg = [
-          segment.image(
-            `https://api.lolimi.cn/API/preview/api.php?qq=${userId}&msg=%E4%BD%A0%E5%A5%BD&msg2=%E7%AC%A8%E8%9B%8B&type=102`
-          ),
-          `不加好友不赞🙄`,
-        ];
-        e.reply(msg, true, { recallMsg: 60 });//60秒后撤回
-        return;
-      }
-    }
+    // if (this.checkFriend) {
+    //   // let isFriend = await (e.bot ?? Bot).fl.get(userId);
+    //   // console.log(isFriend)
+    //   if (!isFriend) {
+    //     let msg = [
+    //       // segment.image(
+    //       //   `https://api.lolimi.cn/API/preview/api.php?qq=${userId}&msg=%E4%BD%A0%E5%A5%BD&msg2=%E7%AC%A8%E8%9B%8B&type=102`
+    //       // ),
+    //       `不加好友不赞🙄`,
+    //     ];
+    //     e.reply(msg, true, { recallMsg: 60 });//60秒后撤回
+    //     return;
+    //   }
+    // }
     let isSuccess = await e.bot.pickFriend(e.user_id).thumbUp(10);
     if (isSuccess.status == "ok") {
       let msg = [
-        segment.image(
-          `https://api.lolimi.cn/API/preview/api.php?qq=${userId}&msg=%E4%BD%A0%E5%A5%BD&msg2=%E7%AC%A8%E8%9B%8B&type=29`
-        ),
+        // segment.image(
+        //   `https://api.lolimi.cn/API/preview/api.php?qq=${userId}&msg=%E4%BD%A0%E5%A5%BD&msg2=%E7%AC%A8%E8%9B%8B&type=29`
+        // ),
         `我已经赞你10次,记得回赞哦！`,
       ];
       e.reply(msg, true, { recallMsg: 60 });//60秒后撤回
     } else {
       let msg = [
-        segment.image(
-          `https://api.lolimi.cn/API/preview/api.php?qq=${userId}&msg=%E4%BD%A0%E5%A5%BD&msg2=%E7%AC%A8%E8%9B%8B&type=66`
-        ),
+        // segment.image(
+        //   `https://api.lolimi.cn/API/preview/api.php?qq=${userId}&msg=%E4%BD%A0%E5%A5%BD&msg2=%E7%AC%A8%E8%9B%8B&type=66`
+        // ),
         `笨蛋今天已经赞过了!`,
       ];
       e.reply(msg, true, { recallMsg: 60 });//60秒后撤回
